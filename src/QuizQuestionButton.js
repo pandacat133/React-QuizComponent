da73e232c6f01c7ec7e3269d3b5adfed2b74d0df
Celie Baker <celie.n133@gmail.com>
2018-07-28 17:23:17 -0600

    fix

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component{
-    button_text = this.props.quiz_question.answer_options[0];
 
     render() {
         return(
             <li>
                 <button>
-                    {this.button_text}
+                    {this.props.button_text}
                 </button>
             </li>
         );
