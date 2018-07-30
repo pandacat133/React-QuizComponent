93fcb9f6ed1741ff2aa976de96fea71361121403
Celie Baker <celie.n133@gmail.com>
2018-07-30 08:33:16 -0600

    fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,8 +12,8 @@ class Quiz extends Component {
     }
 
     render() {
-        // const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
-        const isQuizEnd = true;
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+        // const isQuizEnd = true;
 
         return (
             <div>
